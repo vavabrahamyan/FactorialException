84e782d18511ab0fc5942bb519c8664167284b71
Vahe Abrahamyan <vavabrahamyan@gmail.com>
2020-12-21 09:47:32 +0400

    Добавьте файлы проекта.

---
+++ FactorialException/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace FactorialException
+{
+    public class MyMathFunctions
+    {
+        public static int Factorial(int value)
+        {
+            if (value < 0)
+            {
+                string s = String.Format(
+                     "Illegal negative argument to Factorial {0}", value);
+
+                throw new ArgumentException(s);
+            }
+
+            int factorial = 1;
+
+            do
+            {
+                factorial *= value;
+            } while (--value > 1);
+
+            return factorial;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                for (int i = 6; i > -6; i--)
+                {
+                    int factorial = MyMathFunctions.Factorial(i);
+
+                    Console.WriteLine("i = {0}, factorial = {1}",
+                                      i, factorial);
+                }
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Fatal error:");
+
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("Press Enter to terminate...");
+            Console.Read();
+        }
+    }
+}
